86f1d87be1893ab56d8c83e9d9d685c1c3fb1d4b
DucelierM <mickaelducelier@hotmail.fr>
2018-01-24 22:53:44 +0100

    Ajout de la Scene sur la fenêtre

--- src/com/pacmanrevolution/game/Main.java
+++ src/com/pacmanrevolution/game/Main.java
@@ -1,3 +1,37 @@
+<<<<<<< HEAD
+package com.pacmanrevolution.game;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static String namePlayer;
+
+	public static void main(String[] args) {
+		Scene scene;
+		String namePlayer;
+		
+		JOptionPane.showMessageDialog(null, "Bienvenue sur PacMan Révolution ! Ne rage pas trop si tu n'arrive pas a finir le jeu !");
+		
+		String name = JOptionPane.showInputDialog(null, "Veuillez indiquez votre nom !", 
+	    		"Informations personnel", JOptionPane.QUESTION_MESSAGE);
+	    namePlayer = name;
+	    JOptionPane.showMessageDialog(null, "Votre nom est " + name, "Identité", JOptionPane.INFORMATION_MESSAGE);
+
+		
+		JFrame display = new JFrame("Pac Man Révolution");
+
+		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		display.setSize(700, 700);
+		display.setLocationRelativeTo(null);
+		display.setResizable(false);
+		display.setAlwaysOnTop(true);
+		scene = new Scene();
+		display.setContentPane(scene);
+		display.setVisible(true);
+	}
+}
+=======
 package com.pacmanrevolution.game;
 
 import javax.swing.JFrame;
@@ -25,3 +59,4 @@ public class Main {
 		display.setVisible(true);
 	}
 }
+>>>>>>> fc39f1a918267727ee36c781dc57dcb849d57640
