8c22cb960ed324e5e30f40cf47ce649773227f46
Pouxbelle <35077081+Pouxbelle@users.noreply.github.com>
2018-02-02 10:38:02 +0100

    maj 2
    // pacman se deplace !

--- src/com/pacmanrevolution/game/Control.java
+++ src/com/pacmanrevolution/game/Control.java
@@ -1,5 +1,47 @@
 package com.pacmanrevolution.game;
 
-public class Control {
+import java.awt.Button;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import com.pacmanrevolution.characters.PacMan;
+
+public class Control implements KeyListener{
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			Main.sc1.pac.setMove("RIGHT");
+		}
+		
+		 if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			 Main.sc1.pac.setMove("LEFT");
+		}
+
+		 if (e.getKeyCode() == KeyEvent.VK_UP) {		
+			 Main.sc1.pac.setMove("UP");
+		}
+
+		 if (e.getKeyCode() == KeyEvent.VK_DOWN) {		
+			 Main.sc1.pac.setMove("DOWN");
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
--- src/com/pacmanrevolution/game/Scene.java
+++ src/com/pacmanrevolution/game/Scene.java
@@ -17,30 +17,36 @@ public class Scene extends JPanel {
 
 	private ImageIcon icoFond = new ImageIcon("sprites/Wall/Background.jpg");
 	private Image imgFond1 = icoFond.getImage();
-	
 	public PacMan pac = new PacMan();
 
 	public Scene() {
 
 		
 		
-		Thread chronoEcran = new Thread(new Chrono());
+		Thread chronoEcran = new Thread(new PacMan());
 		chronoEcran.start();
 		
 		this.setFocusable(true);
 		this.requestFocusInWindow();
-		this.addKeyListener(new PacMan());
-	
+		this.addKeyListener(new Control());
+		
+
+		
 	}
 
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {  // methode d'affichage de la map et de tous es elements 
 		super.paintComponent(g);
 		Graphics g2 = (Graphics2D) g; // Graphics2D donne un meilleur rendu graphique
+		
+		this.pac.deplacerPacMan();	//dessine pac Man
+		
 		g2.drawImage(this.imgFond1, 0, 0, null); // dessine l'arriere plan
 		g2.drawImage(pac.getElementImg(),pac.getCharacterX(),pac.getCharacterY(), null); // dessine pacman
 
 		g2.drawString("Est tu pret a perdre", 100, 650);
 
 	}
+
+
 
 }
