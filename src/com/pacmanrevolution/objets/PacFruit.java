b3e574a05331fb3c616141197755362a6d7b7fd0
DucelierM <mickaelducelier@hotmail.fr>
2018-02-05 23:26:40 +0100

    Affichage de l'item

--- src/com/pacmanrevolution/objets/PacFruit.java
+++ src/com/pacmanrevolution/objets/PacFruit.java
@@ -1,5 +1,12 @@
 package com.pacmanrevolution.objets;
 
-public class PacFruit {
+import javax.swing.ImageIcon;
 
+public class PacFruit extends Item{
+
+	public PacFruit(int elementX, int elementY, String fruit) {
+		super(30 , 30 , elementX, elementY);
+		super.icoObjet = new ImageIcon("sprites/Fruit/"+fruit+".png");
+	    super.imgObjet = super.icoObjet.getImage();
+	}
 }
--- src/com/pacmanrevolution/objets/PacGum.java
+++ src/com/pacmanrevolution/objets/PacGum.java
@@ -1,5 +1,15 @@
 package com.pacmanrevolution.objets;
 
-public class PacGum {
+import javax.swing.ImageIcon;
+
+public class PacGum extends Item {
+
+	public PacGum(int elementX, int elementY) {
+		super(30 , 30 , elementX, elementY);
+		super.icoObjet = new ImageIcon("sprites/PacItems/PacGum.jpg");
+	    super.imgObjet = super.icoObjet.getImage();
+	}
+	
+	
 
 }
