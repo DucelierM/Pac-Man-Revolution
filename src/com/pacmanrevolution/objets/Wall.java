597141d033fcfe0b360a3068863d4ef646030b3b
DucelierM <mickaelducelier@hotmail.fr>
2018-02-05 23:27:20 +0100

    Début de collision

--- src/com/pacmanrevolution/objets/Wall.java
+++ src/com/pacmanrevolution/objets/Wall.java
@@ -1,5 +1,23 @@
 package com.pacmanrevolution.objets;
 
-public class Wall {
+import javax.swing.ImageIcon;
 
+import com.pacmanrevolution.game.Element;
+
+public class Wall extends Element{
+
+	public Wall() {
+		this.imgElement = "sprites/Wall1/Wall100.jpg";
+		this.idAnimationImgElement = 0;
+		
+		this.elementIco = new ImageIcon(imgElement);
+		this.elementImg= elementIco.getImage();
+		
+		this.elementX=250;
+		this.elementY=250;
+		
+		this.elementHeight=35;
+		this.elementLength=35;
+		
+	}
 }
