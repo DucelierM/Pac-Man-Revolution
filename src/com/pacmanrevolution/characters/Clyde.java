058f16958e89896a56fbac845a760a2c02b0e8d2
Pouxbelle <35077081+Pouxbelle@users.noreply.github.com>
2018-02-14 18:37:22 +0100

    v 4.1

--- src/com/pacmanrevolution/characters/Clyde.java
+++ src/com/pacmanrevolution/characters/Clyde.java
@@ -11,7 +11,7 @@ public class Clyde extends Ghost{
 	
 		public Clyde( int elementX,int elementY) {
 
-			super(new ImageIcon("sprites/PacMan/PacMan0.png"),"sprites/PacMan/PacMan0.png",
+			super(new ImageIcon("sprites/Clyde/Clyde0.png"),"sprites/Clyde/Clyde0.png",
 					elementX,elementY,35,35,0,25,"RIGHT","0");
 			
 			super.elementImg=elementIco.getImage();
@@ -42,8 +42,6 @@ public class Clyde extends Ghost{
 					else if (this.idAnimationImgElement==0 || this.idAnimationImgElement == 8 ) {
 						this.elementRefImg = "sprites/Clyde/Clyde1.png";
 					}
-				//	else { this.move = "DOWN";}
-				
 			}
 			
 			else if (move =="LEFT") {
@@ -54,7 +52,6 @@ public class Clyde extends Ghost{
 					else if (this.idAnimationImgElement==0 || this.idAnimationImgElement == 8 ) {
 						this.elementRefImg = "sprites/Clyde/Clyde3.png";	
 					}	
-				//	else { this.move = "UP";}
 			}
 			
 			
@@ -65,7 +62,6 @@ public class Clyde extends Ghost{
 					else if (this.idAnimationImgElement==0 || this.idAnimationImgElement == 8 ) {
 						this.elementRefImg = "sprites/Clyde/Clyde5.png";		
 					}
-				//	else { move = "RIGHT";}
 			}
 			
 			
@@ -77,7 +73,6 @@ public class Clyde extends Ghost{
 					else if (this.idAnimationImgElement==0 || this.idAnimationImgElement == 8 ) {
 						this.elementRefImg = "sprites/Clyde/Clyde7.png";
 					}
-				//	else { move = "LEFT" ;}
 			}
 			
 		
@@ -95,20 +90,75 @@ public class Clyde extends Ghost{
 		}
 		
 	//IAclyde
-		
-		public void iaClyde() {
+
+		public void iaClyde(PacMan pac) {
 			
-			if (this.elementX == 662)
-				{this.move = "DOWN";}
+	/*	
+		//changement de direction lorsqu'il est dans un mur
+			
+			if(this.elementY == 638 && this.move == "DOWN") {
+				this.move="RIGHT";
+			}
+			
+			else if(this.elementY == 0 && this.move == "UP") {
+				this.move="LEFT";
+			}
+			
+			else if(this.elementX == 0 && this.move == "LEFT") {
+				this.move="DOWN";
+			}
+			
+			else if(this.elementX == 662 && this.move == "RIGHT") {
+				this.move="UP";
+			}
+	*/	
+		
+		//clyde se dirige vers pac man
+		
+			if ( this.getElementX() > pac.getElementX())	//clyde se trouve a droite de pac man
+				{this.setMove("LEFT");} 
 				
-			if (this.elementX == 0 )	
-				{this.move = "UP";}
-				
-			 if(this.elementY == 0)
-			 	{this.move = "RIGHT";}
-				
-			 if(this.elementY == 638)
-			 	{this.move = "LEFT";}
-		}
+			else if(this.getElementY()< pac.getElementY())	//clyde se trouve au dessus  de pac man
+				{this.setMove("DOWN");} 
+						
+			else if(this.getElementY() > pac.getElementY())		//clyde se trouve en dessous  de pac man
+				{this.setMove("UP");} 
+
+			else if (this.getElementX()< pac.getElementX())		//clyde se trouve a gauche de pac man		
+				{this.setMove("RIGHT");} 
+			
+			
+			
+		}	
+			
+		public void recoveryClyde(PacMan pac) {	
+			
+		/*	//clyde touche pac man et il y a un recovery time 
+			
+			 if (this.getElementY()+this.getElementHeight() == pac.getElementY()){	
+				 this.setMove("UP");													//contact  du mur par le haut
+			}
+			
+			else if(this.elementX+this.elementLength == pac.getElementX()){	
+					this.setMove("LEFT");												//contact  du mur par la gauche	
+			}
+			
+					
+			else if(this.getElementX() == pac.getElementX()+pac.getElementLength()){	//contact  du mur par la droite
+					this.setMove("RIGHT");
+			} 
+
+			else if (this.getElementY() == pac.getElementY()+pac.getElementHeight()){	//contact  du mur par le bas
+					this.setMove("DOWN");}
+			 
+			
+			 for(int i =0;i<5000; i++){
+				 this.moveCharacter();
+			}
+			*/
+	}
+	
+		
+		
 	
 }
