db21f615b94348004f6de08a8faea2ae3ef81065
DucelierM <mickaelducelier@hotmail.fr>
2018-02-21 20:41:40 +0100

    v4.2 Permet d'afficher un score

--- src/com/pacmanrevolution/display/Score.java
+++ src/com/pacmanrevolution/display/Score.java
@@ -1,5 +1,57 @@
 package com.pacmanrevolution.display;
 
-public class Score {
+public abstract class Score {
+	private int scoreFruit = 50; 
+	private int scoreGhost = 150 ;
+	private  int scorePacGum = 100;
+	private  int scoreLife = 0 ;
+	private  boolean haveUsedContinues;
+	private String namePlayer;
+
+	public Score() {
+		this.scoreLife = 0;
+		
+	}
+
+	public void showScore () {
+	}
+
+	public String saveScore() {
+		return null;	
+	}
+
+	public int getScoreLife() {
+		return scoreLife;
+	}
+
+	public void setScoreLife(int scoreLife) {
+		this.scoreLife = scoreLife;
+	}
+
+	public String getNamePlayer() {
+		return namePlayer;
+	}
+
+	public void setNamePlayer(String namePlayer) {
+		this.namePlayer = namePlayer;
+	}
+
+	public int getScoreFruit() {
+		return scoreFruit;
+	}
+
+	public int getScoreGhost() {
+		return scoreGhost;
+	}
+
+	public int getScorePacGum() {
+		return scorePacGum;
+	}
+
+	public boolean isHaveUsedContinues() {
+		return haveUsedContinues;
+	}
+	
+	
 
 }
